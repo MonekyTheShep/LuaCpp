fde7dd82f6a402158c57c9d4cb87a4e2b19d306c
Moneky <237399958+MonekyTheShep@users.noreply.github.com>
2026-08-08 19:33:14 +0100

    Update stdlib.cpp

--- src/vm/stdlib/stdlib.cpp
+++ src/vm/stdlib/stdlib.cpp
@@ -20,16 +20,16 @@ struct Lib
     std::unique_ptr<Library> handle;
 };
 
-static std::array<Lib, 4> libraries =
-{{
-    {"_G", std::make_unique<BaseLib>()},
-    {"table", std::make_unique<TableLib>()},
-    {"string", std::make_unique<StringLib>()},
-    {"io", std::make_unique<IoLib>()},
-}};
-
 void StdLib::initLibraries(VM &vm)
 {
+    std::array<Lib, 4> libraries =
+    {{
+        {"_G", std::make_unique<BaseLib>()},
+        {"table", std::make_unique<TableLib>()},
+        {"string", std::make_unique<StringLib>()},
+        {"io", std::make_unique<IoLib>()},
+    }};
+
     for (const Lib &lib : libraries)
     {
        vm.globals->set(vm, lib.name, lib.handle->openLibrary(vm));
