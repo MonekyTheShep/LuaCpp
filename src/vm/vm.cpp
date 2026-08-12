4a3fc5918523be51fb29a99798683ea50af2f049
Moneky <237399958+MonekyTheShep@users.noreply.github.com>
2026-08-12 23:38:39 +0100

    Update vm.cpp

--- src/vm/vm.cpp
+++ src/vm/vm.cpp
@@ -523,23 +523,18 @@ void VM::handleBitWise(ByteCode::Op op, Meta::Method method)
             }
         };
 
-        auto toDouble = [](int32_t a) 
-        {
-            return static_cast<double>(a);
-        };
-
         switch (op)
         {
             case ByteCode::Op::BIT_AND: 
-                push(toDouble(BIT_OP(&, iLhs, iRhs))); break;
+                push(static_cast<double>(BIT_OP(&, iLhs, iRhs))); break;
             case ByteCode::Op::BIT_OR: 
-                push(toDouble(BIT_OP(|, iLhs, iRhs))); break;
+                push(static_cast<double>(BIT_OP(|, iLhs, iRhs))); break;
             case ByteCode::Op::BIT_XOR: 
-                push(toDouble(BIT_OP(&, iLhs, iRhs))); break;
+                push(static_cast<double>(BIT_OP(&, iLhs, iRhs))); break;
             case ByteCode::Op::BITSHIFT_LEFT: 
-                push(toDouble(bitShift(iLhs, iRhs))); break;
+                push(static_cast<double>(bitShift(iLhs, iRhs))); break;
             case ByteCode::Op::BITSHIFT_RIGHT: 
-                push(toDouble(bitShift(iLhs, -iRhs))); break;
+                push(static_cast<double>(bitShift(iLhs, -iRhs))); break;
             default:
                 assert(false); // Unreachable
         }
