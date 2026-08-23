e0274f307b0acfa6a201e9f08c356e4b08df28ef
Moneky <237399958+MonekyTheShep@users.noreply.github.com>
2026-08-23 12:26:00 +0100

    Update token.h

--- include/lexer/token.h
+++ include/lexer/token.h
@@ -7,6 +7,105 @@
 struct Token
 {
     public:
+        Token() = default;
+
+        struct Value;
+
+        Token(Value value, int line, int col)
+            : lexeme(value.lexeme)
+            , type(value.type)
+            , line(line)
+            , col(col) {}
+
+        enum class Type : uint8_t;
+
+        static std::string toString(Type type)
+        {
+            switch (type)
+            {
+                // KEYWORDS
+                case Type::LOCAL: return "local";
+                case Type::GOTO: return "goto";
+                case Type::FUNCTION: return "function";
+                case Type::THEN: return "then";
+                case Type::END: return "end";
+                case Type::IN: return "in";
+                case Type::NIL: return "nil";
+                case Type::IF: return "if";
+                case Type::ELSE: return "else";
+                case Type::ELSE_IF: return "elseif";
+                case Type::FOR: return "for";
+                case Type::WHILE: return "while";
+                case Type::REPEAT: return "repeat";
+                case Type::UNTIL: return "until";
+                case Type::DO: return "do";
+                case Type::AND: return "and";
+                case Type::OR: return "or";
+                case Type::NOT: return "not";
+                case Type::RETURN: return "return";
+                case Type::BREAK: return "break";
+                case Type::IDENTIFIER: return "<identifier>";
+
+                // TYPES
+                case Type::STRING_LITERAL: return "<string>";
+                case Type::MALFORMED_STRING: return "<malformed string>";
+                case Type::NUMBER_LITERAL: return "<number>";
+                case Type::FALSE: return "false";
+                case Type::TRUE: return "true";
+
+                // ARITHMETIC OPERATORS
+                case Type::OP_ADD: return "+";
+                case Type::OP_SUB: return "-";
+                case Type::OP_MUL: return "*";
+                case Type::OP_DIV: return "/";
+                case Type::OP_FLOOR_DIV: return "//";
+                case Type::OP_MOD: return "%";
+                case Type::OP_EXPO: return "^";
+
+                // ASSIGNMENT OPERATORS
+                case Type::OP_ASSIGN: return "=";
+
+                // COMPARISON OPERATORS
+                case Type::OP_EQUAL: return "==";
+                case Type::OP_NOT_EQUAL: return "~=";
+                case Type::OP_GREATER: return ">";
+                case Type::OP_LESS: return "<";
+                case Type::OP_GREATER_EQUAL: return ">=";
+                case Type::OP_LESS_EQUAL: return "<=";
+
+                // BITWISE OPERATORS
+                case Type::OP_AND: return "&";
+                case Type::OP_OR: return "|";
+                case Type::OP_BITSHIFT_RIGHT: return ">>";
+                case Type::OP_BITSHIFT_LEFT: return "<<";
+                case Type::OP_NOT: return "~";
+
+                // MISC OPERATOR
+                case Type::OP_LENGTH: return "#";
+                case Type::OP_CONCAT: return "..";
+
+                // DELIMITERS
+                case Type::LEFT_PAREN: return "(";
+                case Type::RIGHT_PAREN: return ")";
+                case Type::LEFT_BRACE: return "{";
+                case Type::RIGHT_BRACE: return "}";
+                case Type::LEFT_BRACKET: return "[";
+                case Type::RIGHT_BRACKET: return "]";
+                case Type::COMMA: return ",";
+                case Type::DOT: return ".";
+                case Type::VARARG: return "...";
+                case Type::COLON: return ":";
+                case Type::SEMICOLON: return ";";
+                case Type::DOUBLECOLON: return "::";
+                
+                case Type::MALFORMED_COMMENT: return "<malformed comment>";
+                case Type::COMMENT: return "<comment>";
+                case Type::TEOF: return "<eof>";
+                default:
+                    return "<unknown>";
+            }
+        }
+
         enum class Type : uint8_t
         {
             // KEYWORDS
@@ -97,101 +196,6 @@ struct Token
             std::string_view lexeme;
         };
 
-    public:
-        Token() = default;
-
-        Token(Value value, int line, int col)
-            : lexeme(value.lexeme)
-            , type(value.type)
-            , line(line)
-            , col(col) {}
-
-        static std::string toString(Type type)
-        {
-            switch (type)
-            {
-                // KEYWORDS
-                case Type::LOCAL: return "local";
-                case Type::GOTO: return "goto";
-                case Type::FUNCTION: return "function";
-                case Type::THEN: return "then";
-                case Type::END: return "end";
-                case Type::IN: return "in";
-                case Type::NIL: return "nil";
-                case Type::IF: return "if";
-                case Type::ELSE: return "else";
-                case Type::ELSE_IF: return "elseif";
-                case Type::FOR: return "for";
-                case Type::WHILE: return "while";
-                case Type::REPEAT: return "repeat";
-                case Type::UNTIL: return "until";
-                case Type::DO: return "do";
-                case Type::AND: return "and";
-                case Type::OR: return "or";
-                case Type::NOT: return "not";
-                case Type::RETURN: return "return";
-                case Type::BREAK: return "break";
-                case Type::IDENTIFIER: return "<identifier>";
-
-                // TYPES
-                case Type::STRING_LITERAL: return "<string>";
-                case Type::MALFORMED_STRING: return "<malformed string>";
-                case Type::NUMBER_LITERAL: return "<number>";
-                case Type::FALSE: return "false";
-                case Type::TRUE: return "true";
-
-                // ARITHMETIC OPERATORS
-                case Type::OP_ADD: return "+";
-                case Type::OP_SUB: return "-";
-                case Type::OP_MUL: return "*";
-                case Type::OP_DIV: return "/";
-                case Type::OP_FLOOR_DIV: return "//";
-                case Type::OP_MOD: return "%";
-                case Type::OP_EXPO: return "^";
-
-                // ASSIGNMENT OPERATORS
-                case Type::OP_ASSIGN: return "=";
-
-                // COMPARISON OPERATORS
-                case Type::OP_EQUAL: return "==";
-                case Type::OP_NOT_EQUAL: return "~=";
-                case Type::OP_GREATER: return ">";
-                case Type::OP_LESS: return "<";
-                case Type::OP_GREATER_EQUAL: return ">=";
-                case Type::OP_LESS_EQUAL: return "<=";
-
-                // BITWISE OPERATORS
-                case Type::OP_AND: return "&";
-                case Type::OP_OR: return "|";
-                case Type::OP_BITSHIFT_RIGHT: return ">>";
-                case Type::OP_BITSHIFT_LEFT: return "<<";
-                case Type::OP_NOT: return "~";
-
-                // MISC OPERATOR
-                case Type::OP_LENGTH: return "#";
-                case Type::OP_CONCAT: return "..";
-
-                // DELIMITERS
-                case Type::LEFT_PAREN: return "(";
-                case Type::RIGHT_PAREN: return ")";
-                case Type::LEFT_BRACE: return "{";
-                case Type::RIGHT_BRACE: return "}";
-                case Type::LEFT_BRACKET: return "[";
-                case Type::RIGHT_BRACKET: return "]";
-                case Type::COMMA: return ",";
-                case Type::DOT: return ".";
-                case Type::VARARG: return "...";
-                case Type::COLON: return ":";
-                case Type::SEMICOLON: return ";";
-                case Type::DOUBLECOLON: return "::";
-                
-                case Type::MALFORMED_COMMENT: return "<malformed comment>";
-                case Type::COMMENT: return "<comment>";
-                case Type::TEOF: return "<eof>";
-                default:
-                    return "<unknown>";
-            }
-        }
 
     public:
         std::string_view lexeme;
